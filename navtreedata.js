a0581861389b888086ccb683e59a46e68c1bd35e
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-20 21:04:15 +0200

    Docs for deki-fsm from deki-editor d152da0

--- navtreedata.js
+++ navtreedata.js
@@ -31,8 +31,10 @@ var NAVTREE =
       [ "FsmComponent", "index.html#c-fsmcomponent", null ]
     ] ],
     [ "Changelog", "index.html#autotoc_md3", [
-      [ "0.15.0", "index.html#autotoc_md4", null ],
-      [ "Changed", "index.html#autotoc_md5", null ]
+      [ "Unreleased", "index.html#autotoc_md4", null ],
+      [ "Added", "index.html#autotoc_md5", null ],
+      [ "0.16.0", "index.html#autotoc_md6", null ],
+      [ "Changed", "index.html#autotoc_md7", null ]
     ] ]
   ] ]
 ];
